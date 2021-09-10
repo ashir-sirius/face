f67f57f81db33cf7f27235dcb959300a2db3b9ad
mashirali <57099739+mashirali@users.noreply.github.com>
2021-09-10 18:17:10 +0500

    update 1.0.2

--- app.py
+++ app.py
@@ -4,26 +4,29 @@ from faces import detect_face
 
 app = Flask(__name__)
 
-
-@app.route('/', methods=['GET'])
-def start():
-    return render_template('index.html')
-
-
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET','POST'])
 def predict():
-    imagefile1 = request.files['imagefile1']
-    imagefile2 = request.files['imagefile2']
+    
+    if request.method == 'GET':
+        return render_template('index.html')
+    
+    if request.method == 'POST':
+        imagefile1 = request.files['imagefile1']
+        imagefile2 = request.files['imagefile2']
 
-    image_path1 = "./images/" + imagefile1.filename
-    imagefile1.save(image_path1)
+        image_path1 = "./images/" + imagefile1.filename
+        imagefile1.save(image_path1)
 
-    image_path2 = "./images/" + imagefile2.filename
-    imagefile2.save(image_path2)
+        image_path2 = "./images/" + imagefile2.filename
+        imagefile2.save(image_path2)
 
-    results = detect_face(image_path1, image_path2)
-
-    return render_template('index.html', prediction=results)
+        files = request.files.getlist("images")
+    
+        results = detect_face(image_path1, image_path2)
+    
+        print(results)
+    
+        return render_template('index.html', prediction=results)
 
 
 if __name__ == '__main__':
